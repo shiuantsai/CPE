aec79a067338e723ab97548572174e6c34e91245
unknown <mnhc831231@gmail.com>
2018-08-10 10:45:34 +0800

    uva10783 - OddSum

---
+++ uva10783 - OddSum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main{
+	public static void main(String args[]){
+		Scanner scn = new Scanner(System.in);
+		
+		
+		int n = scn.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			int a = scn.nextInt();
+			int b = scn.nextInt();
+			
+			/*int temp=0;
+			if(a>b){
+				temp = a;
+				a = b;
+				b = temp;
+			}*/
+			
+			int sum=0;
+			for(int j=a;j<=b;j++){
+				if(j%2==1)
+					sum+=j;
+			}
+			System.out.println("Case "+(i+1)+": "+sum);
+		}
+	}
+}
