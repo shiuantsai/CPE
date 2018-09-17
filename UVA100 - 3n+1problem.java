387e3e1c1053fd8a02c4affefd4f2dc4bdf0adf9
shiuantsai <a5204860@gmail.com>
2018-09-17 15:10:34 +0800

    Update UVA100 - 3n+1problem.java

--- UVA100 - 3n+1problem.java
+++ UVA100 - 3n+1problem.java
@@ -1,55 +1,38 @@
-/*輸入兩個數i,j
-介於i,j之間最大數的循環長度為何
-規則:
-如果n不等於1則:
-如果是奇數3n+1 否則n/2
-計算此迴圈共需要算幾次
-*/
+/*照著該規則走，算出該範圍內有多少個數字*/
 import java.util.*;
- class main{
-	public static void main(String[] args){
-		Scanner scn=new Scanner(System.in);
-		
+class main{
+	public static void main(String args[]){
+		Scanner scn = new Scanner(System.in);
 		while(scn.hasNext()){
-			int input1=scn.nextInt();
-			int input2=scn.nextInt();
-			int aa=input1;
-			int bb=input2;
+			int a = scn.nextInt();
+			int b = scn.nextInt();
+			int aa = a;
+			int bb = b;
 			
 			int temp;
-			//使input1較小
-			if(input1 > input2){
-				temp=input1;
-				input1=input2;
-				input2=temp;
+			if(a>b){
+				temp=a;
+				a=b;
+				b=temp;
 			}
-			int max=0;
-			for(int i=input1;i<=input2;i++){
 			
-				int count=1;
-				int incalculate=i;
-				
-				while(true)
-				{
-					if (incalculate==1)
-						break;
-					if(incalculate%2!=0)
-						incalculate=3*incalculate+1;
+			int max=0;
+			for(int i=a;i<=b;i++){
+			int count=1;
+			int compute = i;
+				while(compute!=1){
+				count++;
+					if(compute%2==1)
+						compute = 3*compute+1;
 					else
-						incalculate=incalculate/2;
-					count++;
+					   compute = compute/2;
 				}
-				
-				if(count > max)
+				if(count>max){
 					max=count;
+				}
 				
 			}
 			System.out.println(aa+" "+bb+" "+max);
-		
-		
 		}
-		
-		
 	}
-	
 }
